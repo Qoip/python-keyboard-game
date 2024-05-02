78a4ebf53803b3d0ff1739c47e11347ca455d1a8
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-03 00:52:34 +0300

    Game needed fields

--- bin/vertex.py
+++ bin/vertex.py
@@ -2,11 +2,14 @@
 
 
 class Vertex:
-    def __init__(self, x: int, y: int, owner: str | None = None, is_main: bool = False):
+    def __init__(self, x: int, y: int, owner: str | None = None, is_main: bool = False, size: int = 5, hp: int = 10):
         self.x = x
         self.y = y
         self.owner = owner
         self.is_main = is_main
+        self.size = size
+        self.hp = hp
+        self.name: str = "no name"
 
     def __str__(self):
-        return f"vertex at ({self.x}, {self.y}), owned by {self.owner}"
+        return f"vertex ({self.x}, {self.y}), owned by {self.owner} ({self.is_main}), size: {self.size}, hp: {self.hp}"
