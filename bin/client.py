7e1afde1dc91733f359e4c3fa4cf9bd078894fcb
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-05 03:32:59 +0300

    client fields

--- bin/client.py
+++ bin/client.py
@@ -1,6 +1,9 @@
-from bin.graph import Graph
+""" Client module """
 
-from typing import Tuple
+from bin.graph import Graph
+from bin.view import View
+
+from typing import Tuple, Dict
 import tkinter as tk
 from tkinter import colorchooser, messagebox
 
@@ -8,17 +11,27 @@ from tkinter import colorchooser, messagebox
 class Client:
     def __init__(self):
         self.graph: Graph = None
+        self.color_scheme: Dict[str, Tuple[int, int, int]] = None
+        self.legend: Dict[str, int] = None
+
         self.nickname: str = None
         self.color: Tuple[int, int, int] = None
         self.address: str = None
+
+    async def run(self):
+        ''' Run client '''
         self.run_menu()
         # self.connect()
+        self.view = View(self.color_scheme, self.graph, self.nickname, self.legend)
+        # self.run_listener()
+        # self.run()
 
     def run_menu(self) -> None:
         ''' Run menu '''
         root = tk.Tk()
-        root.title("Menu")
+        root.title("Client menu")
         root.geometry("200x200")
+        root.resizable(False, False)
 
         nickname_label = tk.Label(root, text="Nickname:")
         nickname_label.pack()
@@ -38,14 +51,14 @@ class Client:
         address_entry.pack()
 
         def save():
-            self.nickname = nickname_entry.get()
-            self.address = address_entry.get()
             if not self.nickname or not self.color or not self.address:
                 messagebox.showerror("Error", "Fill all fields")
                 return
             if len(self.nickname) > 14:
                 messagebox.showerror("Error", "Nickname is too long (max 14)")
                 return
+            self.nickname = nickname_entry.get()
+            self.address = address_entry.get()
             root.destroy()
 
         connect_button = tk.Button(root, text="Save", command=save)
