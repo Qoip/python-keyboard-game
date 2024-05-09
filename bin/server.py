a0f30c33da3bf06d97ec7604a768023f5ef8e9aa
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-10 01:47:20 +0300

    game loop logic

--- bin/server.py
+++ bin/server.py
@@ -20,39 +20,91 @@ class Server:
         self.graph: Graph = None
         self.players: List[str] = []
         self.color_scheme: Dict[str, Tuple[int, int, int]] = {}
-        self.game_start_time: float = None  # also game started flag
+        self.current_vertex: Dict[str, int] = {}  # nickname -> vertex_index
 
         self.port = random.randrange(10000, 60000)
         self.address: str = f"127.0.0.1:{self.port}"
         self.server: asyncio.AbstractServer = None
         self.is_serving: bool = False
         self.active_connections: Set[asyncio.StreamWriter] = set()
+        self.players_address: Dict[Tuple[str, int], str] = {}  # address -> nickname
 
-        self.players_address: Dict[Tuple[str, int], str] = {}
-        self.client_updates: asyncio.Queue[Tuple[str, Dict[str, Any]]] = asyncio.Queue()
+        self.game_start_time: float = None  # also game started flag
+        self.client_updates: asyncio.Queue[Tuple[str, Dict[str, Any]]] = asyncio.Queue()  # nickname -> json query
 
     async def run(self):
         server_thread = threading.Thread(target=lambda: asyncio.run(self.start_server()))
         server_thread.start()
 
         await self.run_menu()
+        print("[main]", "dense:", self.dense, "bounds:", self.bounds, "time:", self.time)
+        print("[main]", "players:", self.players, "color_scheme:", self.color_scheme)
+        print("[main]", "legend:", self.legend, "players_address:", self.players_address)
+
         self.graph = Graph()
         self.graph.generate(self.players, self.bounds, self.dense)
         self.legend: Dict[str, int] = self.get_legend()
+        self.current_vertex = {player: self.graph.get_main(player) for player in self.players}
 
-        print("[settings]", "dense:", self.dense, "bounds:", self.bounds, "time:", self.time)
-        print("[settings]", "players:", self.players, "color_scheme:", self.color_scheme)
-        print("[settings]", "legend:", self.legend, "players_address:", self.players_address)
-
-        # self.game_start_time = asyncio.get_event_loop().time()
-        # while asyncio.get_event_loop().time() - self.game_start_time < self.time:
-        #     await self.game_loop()
+        print("[main]", "game started")
+        self.game_start_time = asyncio.get_event_loop().time()
+        while self.estimated_time() > 0:
+            await self.game_loop()
 
         self.is_serving = False
         server_thread.join()
 
     async def game_loop(self):
         """ Game loop """
+        if self.estimated_time() != self.legend["time"]:
+            self.legend["time"] = self.estimated_time()
+            print("[game]", "time left:", self.estimated_time())
+            for player in self.players:
+                self.legend[player] += self.graph.count(player)
+
+        while self.client_updates.qsize() > 0:
+            nickname, data = await self.client_updates.get()
+            if data.get("command") == "change":
+                try:
+                    vertex_index = int(data.get("argument"))
+                    assert vertex_index < len(self.graph.vertices)
+                    assert vertex_index >= 0
+                    assert self.graph.reachable(nickname, vertex_index)
+                except (ValueError, AssertionError) as exc:
+                    print("[game]", f"invalid change from player {nickname}: {data}\nerror: {exc}")
+                    continue
+                self.current_vertex[nickname] = vertex_index
+                vertex_name = self.graph.vertices[vertex_index].name
+                print("[game]", f"player {nickname} changed current to {vertex_name} ({vertex_index})")
+            elif data.get("command") == "attack":
+                try:
+                    vertex_index = int(data.get("argument"))
+                    assert vertex_index == self.current_vertex[nickname]
+                    assert not self.graph.vertices[vertex_index].is_main
+                except (ValueError, AssertionError) as exc:
+                    print("[game]", f"invalid attack from player {nickname}: {data}\nerror: {exc}")
+                    continue
+                vertex_name = self.graph.vertices[vertex_index].name
+                if self.graph.vertices[vertex_index].owner is nickname:
+                    self.graph.vertices[vertex_index].hp += 1
+                    print("[game]", f"player {nickname} healed vertex {vertex_name} ({vertex_index})")
+                else:
+                    self.graph.vertices[vertex_index].hp -= 1
+                    print("[game]", f"player {nickname} attacked vertex {vertex_name} ({vertex_index})")
+                    if self.graph.vertices[vertex_index].hp == 0:
+                        self.graph.vertices[vertex_index].hp = 3
+                        old_owner = self.graph.vertices[vertex_index].owner
+                        self.graph.vertices[vertex_index].owner = nickname
+                        print("[game]", f"player {nickname} captured vertex {vertex_name} ({vertex_index})")
+
+                        if old_owner is not None:  # check if old owner needs to force change vertex
+                            if not self.graph.reachable(old_owner, self.current_vertex[old_owner]):
+                                self.current_vertex[old_owner] = vertex_index  # move to captured vertex
+                                print("[game]", f"player {old_owner} forced to change vertex")
+
+    def estimated_time(self) -> int:
+        """ Estimated time to end game """
+        return self.time - int(asyncio.get_event_loop().time() - self.game_start_time)
 
     async def start_server(self):
         """ Start server listening """
