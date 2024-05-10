872572ffa76e900fca9148bac4bcde2663432f85
konstantin.kozyrev <kozyrev.kv@phystech.edu>
2024-05-10 15:41:52 +0300

    start file added

---
+++ start.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import asyncio
+
+from bin.client import Client
+from bin.server import Server
+
+
+def start_server(root: tk.Tk):
+    root.destroy()
+    server = Server()
+    asyncio.run(server.run())
+
+
+def start_client(root: tk.Tk):
+    root.destroy()
+    client = Client()
+    asyncio.run(client.run())
+
+
+def main():
+    root = tk.Tk()
+
+    server_button = tk.Button(root, text="Server", command=lambda: start_server(root))
+    server_button.pack()
+    client_button = tk.Button(root, text="Client", command=lambda: start_client(root))
+    client_button.pack()
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
